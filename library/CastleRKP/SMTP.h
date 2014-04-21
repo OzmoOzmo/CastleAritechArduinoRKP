041d21eb6a4263bc86089205d184ff14b52c0b32
OzmoOzmo <ozmo.spam@gmail.com>
2014-04-21 03:51:18 +0100

    Create SMTP.h

---
+++ library/CastleRKP/SMTP.h
@@ -0,0 +1,44 @@
+
+/* 
+* SMTP.h
+*
+* Created: 4/5/2014 7:45:36 PM
+
+*/
+
+
+#ifndef __SMTP_H__
+#define __SMTP_H__
+
+#include <EthernetClient.h>
+
+class SMTP
+{
+//variables
+public:
+	static int nEmailStage;
+protected:
+private:
+	static EthernetClient client;
+	//static int mDelay;
+	static unsigned long mTimeout;
+	static boolean bWaitForResponse;
+	static IPAddress SMTPServer;
+	
+//functions
+public:
+	static void QueueEmail();
+	static void SendEmailProcess();
+	static boolean WaitForReplyLine();
+
+protected:
+private:
+	SMTP();
+	SMTP( const SMTP &c );
+	SMTP& operator=( const SMTP &c );
+	
+	boolean waitForReplyLine();
+	
+}; //SMTP
+
+#endif //__SMTP_H__
