b7b84a52241c411e749742e3a06bba2e7fb2a126
OzmoOzmo <ozmo.spam@gmail.com>
2014-04-21 03:52:24 +0100

    Create WebSocket.h

---
+++ library/CastleRKP/WebSocket.h
@@ -0,0 +1,29 @@
+/*
+ * WebSocket.h
+ *
+ * Created: 3/30/2014 11:54:30 PM Ozmo
+ */ 
+
+#ifndef WEBSOCKET_H_
+#define WEBSOCKET_H_
+
+class WebSocket
+{
+	public:
+		// Create a Websocket server
+		void static WebSocket_EtherInit();
+
+		//Send something to connected browser
+		bool static WebSocket_send(char* data, byte length);
+
+		void static WebSocket_doHandshake();
+
+		byte static ReadNext();
+		bool static RejectBroswerMsg();
+		bool static WebSocket_getFrame();
+		void static EtherPoll();
+	private:
+		void static SendHTMLSite();
+};
+
+#endif /* WEBSOCKET_H_ */
