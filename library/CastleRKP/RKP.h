26bc03e7849918fa407cc6ba549bb00ef468a79b
OzmoOzmo <ozmo.spam@gmail.com>
2014-04-25 01:40:22 +0100

    Update RKP.h

--- library/CastleRKP/RKP.h
+++ library/CastleRKP/RKP.h
@@ -3,7 +3,7 @@
  *
  * Created: 3/30/2014 11:18:18 PM
 
- */ 
+ */
 
 #ifndef RKP_h
 #define RKP_h
@@ -15,21 +15,25 @@
 class RKPClass
 {
 	public:
-		void static loop_PanelMon();
+		bool static loop_PanelMon();
+		void static SendDisplayToClientIfChanged();
 		static void DisplayScreen( byte* msgbuf, int msgbuflen);
 		static char NextKeyPress();
 		static char PopKeyPress();
 		static void PushKey( char param1 );
 		RKPClass();
 		static void SendToPanel( int id, bool bAck);
-static void Init();
-static void SendDisplayToBrowser();
+		static void Init(byte nRKP_ID);
+		static void SendDisplayToBrowser();
 		static String msKeyToSend;
-		
+
 		static bool dateFlash;
 		static byte dispBuffer[];
 		static bool mbIsPanelWarning;
 		static bool mbIsPanelAlarm;
+		static byte mdevID;
+		static bool bScreenHasUpdated;
+		static unsigned long timeToSwitchOffLed;
 };
 //extern RKPClass rkp;
 
